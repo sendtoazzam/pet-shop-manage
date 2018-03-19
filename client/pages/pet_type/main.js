13bd79176c83012701fd62879c572ab00ef883a6
joel <iamjoel007@gmail.com>
2018-03-19 19:39:40 +0800

    add add and remove

--- client/pages/pet_type/main.js
+++ client/pages/pet_type/main.js
@@ -3,7 +3,10 @@
   var app = new Vue({
     el: '#app',
     data: {
-      list: []
+      list: [],
+      isShowDialog: false,
+      type: 'add',
+      model: {},
     },
     methods: {
       // 获取列表
@@ -14,21 +17,45 @@
           }
         })
       },
+      // 新增
+      add() {
+        this.type = 'add'
+        this.model = {}
+        this.isShowDialog = true
+      },
       // 编辑
-      edit(id) {
-
+      edit(model) {
+        this.type = 'edit'
+        this.model = model
+        this.isShowDialog = true
       },
       // 删除
       remove(id) {
         axios.delete(`${window.config.apiPrefix}/${resourceName}/${id}`).then(({data}) => {
-          if(data.errCode == 0) {
-            this.$message({
-              message: '删除成功!',
-              type: 'success'
-            })
-            this.fetchList()
-          }
+          this.handleRes(data, '删除')
         })
+      },
+      confirm() {
+        var data = this.model
+        if(this.type === 'add') {
+          axios.put(`${window.config.apiPrefix}/${resourceName}`, data).then(({data}) => {
+            this.handleRes(data, '新增')
+          })
+        } else {
+          axios.post(`${window.config.apiPrefix}/${resourceName}/${data.id}`, data).then(({data}) => {
+            this.handleRes(data, '编辑')
+          })
+        }
+      },
+      handleRes(data, opName) {
+        if(data.errCode == 0) {
+          this.$message({
+            message: `${opName}成功!`,
+            type: 'success'
+          })
+          this.fetchList()
+          this.isShowDialog = false
+        }
       }
     },
     mounted() {
--- server/app.js
+++ server/app.js
@@ -16,9 +16,8 @@ app.get('/', (req, res) => res.send('It works!'))
 // 跨域头设置
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type, Accept");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 })
