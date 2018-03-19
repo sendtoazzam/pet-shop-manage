a7c83534f72d814015e4ac7e93b9253ef54f3f4b
joel <iamjoel007@gmail.com>
2018-03-19 16:35:31 +0800

    add cross domin header

--- server/app.js
+++ server/app.js
@@ -13,6 +13,17 @@ var pool = mysql.createPool(config.mysql)
 
 app.get('/', (req, res) => res.send('It works!'))
 
+// 跨域头设置
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+})
+
+// 所有的api
 var apis = {
   pet_type: require('./api/pet_type')
 }
